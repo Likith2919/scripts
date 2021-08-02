5fbfbc065dfcbcee517d52d825106898b5687d24
Cloud User <centos@ip-172-31-9-23.ec2.internal>
2021-08-02 11:59:13 +0000

    jenkins with java

---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [-f /usr/local/bin/java.*]
+then
+	ver =$(java -version)
+	echo "java exists"
+else
+	echo "java not found"
+	sudo yum -y install java-1.8.0*-openjdk
+fi
+sudo yum -y install git
+sudo yum -y install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum -y install jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
